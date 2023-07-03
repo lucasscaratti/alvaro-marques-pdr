84b50222975deb64185c7b44a5ad419c6f6e46df
Lucas Scaratti <lukasscaratti@gmail.com>
2023-07-03 16:41:58 -0300

    Ajustes na barra de navegacao e responsividade

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  let overlay = document.getElementById('overlay');
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+    overlay.style.display = 'block';
+  }
+}
+
+function closeMenu () {
+  let overlay = document.getElementById('overlay');
+  let menuMobile = document.querySelector(".mobile-menu");
+
+  overlay.style.display = 'none';
+  menuMobile.classList.remove("open");
+  document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+}
+
+overlay.addEventListener('click', closeMenu);
+
 function scrollToSection(sectionId, event) {
   event.preventDefault();
   var sectionOffset = document.querySelector(sectionId).offsetTop;
@@ -5,42 +29,27 @@ function scrollToSection(sectionId, event) {
   var scrollToPosition = sectionOffset - headerHeight;
 
   window.scrollTo({
-    top: scrollToPosition
+    top: scrollToPosition,
   });
 }
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 400;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 400;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-};
-
-function showMenu() {
-  let menuMobile = document.querySelector('.mobile-menu-icon');
-  let nav = document.querySelector('nav a')
-  if (menuMobile.classList.contains('open')) {
-    menuMobile.classList.remove('open');
-    // nav.classList.remove('open');
-    document.querySelector('.icon').src = "img/menu-icon.svg"
-  } else {
-    menuMobile.classList.add('open');
-    // nav.classList.add('open');
-    document.querySelector('.icon').src = "img/menu-close.svg"
-  }
-}
-
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((links) => {
+          links.classList.remove("active");
+          document
+            .querySelector("header nav a[href*=" + id + "]")
+            .classList.add("active");
+        });
+      }
+    });
+  };
