b0ea6a88323448281a2847df574d9fd89c8d4c11
Lucas Scaratti <lukasscaratti@gmail.com>
2023-07-06 16:51:20 -0300

    Galeria finalizada, falta organizar o codigo

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 function menuShow() {
   let menuMobile = document.querySelector(".mobile-menu");
-  let overlay = document.getElementById('overlay');
+  let overlay = document.getElementById("overlay");
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
     document.querySelector(".icon").src = "/img/menu-icon.svg";
   } else {
     menuMobile.classList.add("open");
     document.querySelector(".icon").src = "/img/menu-close.svg";
-    overlay.style.display = 'block';
+    overlay.style.display = "block";
   }
 }
 
-function closeMenu () {
-  let overlay = document.getElementById('overlay');
+function closeMenu() {
+  let overlay = document.getElementById("overlay");
   let menuMobile = document.querySelector(".mobile-menu");
 
-  overlay.style.display = 'none';
+  overlay.style.display = "none";
   menuMobile.classList.remove("open");
   document.querySelector(".icon").src = "/img/menu-icon.svg";
 }
 
-overlay.addEventListener('click', closeMenu);
+overlay.addEventListener("click", closeMenu);
 
 function scrollToSection(sectionId, event) {
   event.preventDefault();
@@ -35,23 +35,23 @@ function scrollToSection(sectionId, event) {
   setTimeout(closeMenu, 100);
 }
 
-  let sections = document.querySelectorAll("section");
-  let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-  window.onscroll = () => {
-    sections.forEach((sec) => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 400;
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute("id");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 400;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-      if (top >= offset && top < offset + height) {
-        navLinks.forEach((links) => {
-          links.classList.remove("active");
-          document
-            .querySelector("header nav a[href*=" + id + "]")
-            .classList.add("active");
-        });
-      }
-    });
-  };
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
